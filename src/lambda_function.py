fd49eb4f8a413c70084b3972f7657c7bc0412f5b
Daniel Arturo Sanchez <dasdo1@gmail.com>
2026-01-05 11:02:12 -0400

    push

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -12,6 +12,7 @@
 # string, the authorizer function returns an HTTP 401 status code. For any other token value,
 # the authorizer returns an HTTP 500 status code.
 # Note that token values are case-sensitive.
+# code version: 1.0.0
 
 import json
 import lambda_logger_format
